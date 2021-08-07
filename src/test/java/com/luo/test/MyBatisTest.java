9bdc70a8e7e0c4db65f9ecca43755c03df25c71a
root <root@qq.com>
2021-08-07 16:00:06 +0800

    master-1 commit

--- src/test/java/com/luo/test/MyBatisTest.java
+++ src/test/java/com/luo/test/MyBatisTest.java
@@ -22,5 +22,7 @@ public class MyBatisTest {
         System.out.println("hello git 0");
         System.out.println("hello git 1");
         System.out.println("hello git 2");
+
+        System.out.println("hello git master test");
     }
 }
